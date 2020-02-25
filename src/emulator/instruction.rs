156ff8758756c1682a0dd97280c03bcfaa27c725
Jan Lafferton <jla@jcbachmann.de>
2020-02-25 18:02:38 +0100

    Error handling for instruction decoding

--- src/emulator/instruction.rs
+++ src/emulator/instruction.rs
@@ -73,7 +73,7 @@ pub enum Instruction {
 }
 
 impl Instruction {
-    pub fn from(opcode: u16) -> Self {
+    pub fn from(opcode: u16) -> Result<Self, String> {
         // Only accept properly formatted opcodes
         assert_eq!(opcode & 0xC000, 0);
 
@@ -85,42 +85,42 @@ impl Instruction {
                 let sub_selector = (opcode & 0b1111) as u8;
 
                 match selector {
-                    0b0111 => Instruction::AddWf(file_register, destination_flag),
-                    0b0101 => Instruction::AndWf(file_register, destination_flag),
+                    0b0111 => Ok(Instruction::AddWf(file_register, destination_flag)),
+                    0b0101 => Ok(Instruction::AndWf(file_register, destination_flag)),
                     0b0001 => {
                         if destination_flag.0 {
-                            Instruction::ClrF(file_register)
+                            Ok(Instruction::ClrF(file_register))
                         } else {
-                            Instruction::ClrW
+                            Ok(Instruction::ClrW)
                         }
                     }
-                    0b1001 => Instruction::ComF(file_register, destination_flag),
-                    0b0011 => Instruction::DecF(file_register, destination_flag),
-                    0b1011 => Instruction::DecFsz(file_register, destination_flag),
-                    0b1010 => Instruction::IncF(file_register, destination_flag),
-                    0b1111 => Instruction::IncFsz(file_register, destination_flag),
-                    0b0100 => Instruction::IorWf(file_register, destination_flag),
-                    0b1000 => Instruction::MovF(file_register, destination_flag),
+                    0b1001 => Ok(Instruction::ComF(file_register, destination_flag)),
+                    0b0011 => Ok(Instruction::DecF(file_register, destination_flag)),
+                    0b1011 => Ok(Instruction::DecFsz(file_register, destination_flag)),
+                    0b1010 => Ok(Instruction::IncF(file_register, destination_flag)),
+                    0b1111 => Ok(Instruction::IncFsz(file_register, destination_flag)),
+                    0b0100 => Ok(Instruction::IorWf(file_register, destination_flag)),
+                    0b1000 => Ok(Instruction::MovF(file_register, destination_flag)),
                     0b0000 => {
                         if destination_flag.0 {
-                            Instruction::MovWf(file_register)
+                            Ok(Instruction::MovWf(file_register))
                         } else {
                             match sub_selector {
-                                0b0000 => Instruction::Nop,
-                                0b0100 => Instruction::ClearWdt,
-                                0b1001 => Instruction::RetFie,
-                                0b1000 => Instruction::Return,
-                                0b0011 => Instruction::Sleep,
-                                _ => panic!("Unknown opcode: {:04x}", opcode),
+                                0b0000 => Ok(Instruction::Nop),
+                                0b0100 => Ok(Instruction::ClearWdt),
+                                0b1001 => Ok(Instruction::RetFie),
+                                0b1000 => Ok(Instruction::Return),
+                                0b0011 => Ok(Instruction::Sleep),
+                                _ => Err(format!("Unknown opcode: {:04x}", opcode)),
                             }
                         }
                     }
-                    0b1101 => Instruction::RlF(file_register, destination_flag),
-                    0b1100 => Instruction::RrF(file_register, destination_flag),
-                    0b0010 => Instruction::SubWf(file_register, destination_flag),
-                    0b1110 => Instruction::SwapWf(file_register, destination_flag),
-                    0b0110 => Instruction::XorWf(file_register, destination_flag),
-                    _ => panic!("Unknown opcode: {:04x}", opcode),
+                    0b1101 => Ok(Instruction::RlF(file_register, destination_flag)),
+                    0b1100 => Ok(Instruction::RrF(file_register, destination_flag)),
+                    0b0010 => Ok(Instruction::SubWf(file_register, destination_flag)),
+                    0b1110 => Ok(Instruction::SwapWf(file_register, destination_flag)),
+                    0b0110 => Ok(Instruction::XorWf(file_register, destination_flag)),
+                    _ => Err(format!("Unknown opcode: {:04x}", opcode)),
                 }
             }
             InstructionCategory::BitOriented => {
@@ -129,11 +129,11 @@ impl Instruction {
                 let selector = ((opcode >> 10) & 0b11) as u8;
 
                 match selector {
-                    0b00 => Instruction::BcF(file_register, bit_index),
-                    0b01 => Instruction::BsF(file_register, bit_index),
-                    0b10 => Instruction::BtFsc(file_register, bit_index),
-                    0b11 => Instruction::BtFss(file_register, bit_index),
-                    _ => panic!("Unknown opcode: {:04x}", opcode),
+                    0b00 => Ok(Instruction::BcF(file_register, bit_index)),
+                    0b01 => Ok(Instruction::BsF(file_register, bit_index)),
+                    0b10 => Ok(Instruction::BtFsc(file_register, bit_index)),
+                    0b11 => Ok(Instruction::BtFss(file_register, bit_index)),
+                    _ => Err(format!("Unknown opcode: {:04x}", opcode)),
                 }
             }
             InstructionCategory::LiteralOriented => {
@@ -141,14 +141,14 @@ impl Instruction {
                 let selector = ((opcode >> 8) & 0b1111) as u8;
 
                 match selector {
-                    0b0000 | 0b0001 | 0b0010 | 0b0011 => Instruction::MovLw(literal),
-                    0b0100 | 0b0101 | 0b0110 | 0b0111 => Instruction::RetLw(literal),
-                    0b1100 | 0b1101 => Instruction::SubLw(literal),
-                    0b1111 | 0b1110 => Instruction::AddLw(literal),
-                    0b1010 => Instruction::XorLw(literal),
-                    0b1001 => Instruction::AndLw(literal),
-                    0b1000 => Instruction::IorLw(literal),
-                    _ => panic!("Unknown opcode: {:04x}", opcode),
+                    0b0000 | 0b0001 | 0b0010 | 0b0011 => Ok(Instruction::MovLw(literal)),
+                    0b0100 | 0b0101 | 0b0110 | 0b0111 => Ok(Instruction::RetLw(literal)),
+                    0b1100 | 0b1101 => Ok(Instruction::SubLw(literal)),
+                    0b1111 | 0b1110 => Ok(Instruction::AddLw(literal)),
+                    0b1010 => Ok(Instruction::XorLw(literal)),
+                    0b1001 => Ok(Instruction::AndLw(literal)),
+                    0b1000 => Ok(Instruction::IorLw(literal)),
+                    _ => Err(format!("Unknown opcode: {:04x}", opcode)),
                 }
             }
             InstructionCategory::AddressOriented => {
@@ -156,9 +156,9 @@ impl Instruction {
                 let selector = ((opcode >> 11) & 0b1) as u8;
 
                 match selector {
-                    0b0 => Instruction::Call(address),
-                    0b1 => Instruction::Goto(address),
-                    _ => panic!("Unknown opcode: {:04x}", opcode),
+                    0b0 => Ok(Instruction::Call(address)),
+                    0b1 => Ok(Instruction::Goto(address)),
+                    _ => Err(format!("Unknown opcode: {:04x}", opcode)),
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod emulator;
 use emulator::*;
 
 fn main() {
-    println!("{:?}", Instruction::from(0b00_0111_0111_1111));
-    println!("{:?}", Instruction::from(0b01_0000_0111_1111));
-    println!("{:?}", Instruction::from(0b11_1001_1000_0000));
-    println!("{:?}", Instruction::from(0b10_0111_1111_1111));
-    println!("{:?}", Instruction::from(0b00_0000_0000_1001));
+    println!("{:?}", Instruction::from(0b00_0111_0111_1111).unwrap());
+    println!("{:?}", Instruction::from(0b01_0000_0111_1111).unwrap());
+    println!("{:?}", Instruction::from(0b11_1001_1000_0000).unwrap());
+    println!("{:?}", Instruction::from(0b10_0111_1111_1111).unwrap());
+    println!("{:?}", Instruction::from(0b00_0000_0000_1001).unwrap());
 }

38b02cc3da00ea2349e36322c790ccd8a347f2e3
Jan Lafferton <jla@jcbachmann.de>
2020-02-26 10:31:38 +0100

    Implement rom bus

--- src/emulator/rom_bus.rs
+++ src/emulator/rom_bus.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 
+use super::bits::*;
 use super::instruction::*;
 
 pub struct RomBus {
     rom: [u8; 0xffff],
-    cache: HashMap<u8, Instruction>,
+    cache: HashMap<u16, Instruction>,
 }
 
 impl RomBus {
@@ -20,5 +21,26 @@ impl RomBus {
             let rom_addr = addr + starting_address as usize;
             self.rom[rom_addr] = program[addr];
         }
+    }
+
+    pub fn read_instruction(&mut self, address: u16) -> Result<Instruction, String> {
+        if let Some(instr) = self.cache.get(&address) {
+            Ok(*instr)
+        } else {
+            let decoded = Instruction::from(self.read_word(address));
+            if let Ok(instr) = decoded {
+                self.cache.insert(address, instr);
+            }
+            decoded
+        }
+    }
+
+    fn read_byte(&self, address: u16) -> u8 {
+        self.rom[address as usize]
+    }
+
+    fn read_word(&self, address: u16) -> u16 {
+        // TODO: check endianess
+        join_bytes(self.read_byte(address), self.read_byte(address + 1))
     }
 }

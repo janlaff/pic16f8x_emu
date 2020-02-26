e9fd700eb56bc36cd4050904d0bf283867d09122
Jan Lafferton <jla@jcbachmann.de>
2020-02-26 10:56:49 +0100

    Simple Disassembler + Rom boundary checking

--- src/emulator/rom_bus.rs
+++ src/emulator/rom_bus.rs
@@ -5,6 +5,7 @@ use super::instruction::*;
 
 pub struct RomBus {
     rom: [u8; 0xffff],
+    max_rom_addr: u16,
     cache: HashMap<u16, Instruction>,
 }
 
@@ -12,11 +13,13 @@ impl RomBus {
     pub fn new() -> Self {
         Self {
             rom: [0; 0xffff],
+            max_rom_addr: 0,
             cache: HashMap::new(),
         }
     }
 
     pub fn load_program(&mut self, program: &[u8], starting_address: u16) {
+        self.max_rom_addr = starting_address + program.len() as u16 - 1;
         for addr in 0..program.len() {
             let rom_addr = addr + starting_address as usize;
             self.rom[rom_addr] = program[addr];
@@ -24,6 +27,13 @@ impl RomBus {
     }
 
     pub fn read_instruction(&mut self, address: u16) -> Result<Instruction, String> {
+        if address > self.max_rom_addr {
+            return Err(format!(
+                "Tried to execute arbitrary data as code at address {:04x}",
+                address
+            ));
+        }
+
         if let Some(instr) = self.cache.get(&address) {
             Ok(*instr)
         } else {
@@ -33,6 +43,10 @@ impl RomBus {
             }
             decoded
         }
+    }
+
+    pub fn get_max_rom_addr(&self) -> u16 {
+        self.max_rom_addr
     }
 
     fn read_byte(&self, address: u16) -> u8 {
--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,10 @@ fn main() {
 
     let mut data_bus = DataBus::new();
     let mut rom_bus = RomBus::new();
+
+    rom_bus.load_program(include_bytes!("../SimTest01.bin"), 0x0000);
+
+    for i in (0u16..rom_bus.get_max_rom_addr()).step_by(2) {
+        println!("{:04x}: {:?}", i, rom_bus.read_instruction(i).unwrap());
+    }
 }

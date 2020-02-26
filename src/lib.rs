2dc7c2bc331b98ee7479a3929e08c0729e5b80fb
root <root@DESKTOP-62MUD0T.localdomain>
2020-02-26 18:52:06 +0100

    Add npm frontend base

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate log;
+
+extern crate console_log;
+
+mod emulator;
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -9,11 +15,21 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, test123!");
+pub fn initialize_emulator() {
+    console_log::init_with_level(log::Level::Debug).unwrap();
+    info!("Initialising PIC16F8X emulation engine");
+
+    let mut cpu = emulator::CPU::new();
+    cpu.rom_bus
+        .load_program(include_bytes!("../SimTest02.bin"), 0);
+    let (min, max) = cpu.rom_bus.get_rom_boundary();
+
+    while cpu.data_bus.get_pc() <= max {
+        cpu.step();
+    }
 }
---
+++ www/index.js
@@ -0,0 +1,3 @@
+import * as wasm from "pic16f8x_emu";
+
+wasm.initialize_emulator();

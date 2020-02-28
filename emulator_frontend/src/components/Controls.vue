62cef5c54bccacf3129c394f76eb967f60df7561
Jan Lafferton <jla@jcbachmann.de>
2020-02-28 12:57:57 +0100

    better control buttons

--- emulator_frontend/src/components/Controls.vue
+++ emulator_frontend/src/components/Controls.vue
@@ -1,27 +1,18 @@
 <template>
-  <v-btn-toggle class="ma-5">
-    <v-btn fab small color="primary" @click="$root.$emit('play')">
-      <v-icon color="white">mdi-play</v-icon>
+  <div class="ma-5">
+    <v-btn rounded color="primary" @click="$root.$emit('play')" class="mr-1">
+      <v-icon color="white">mdi-play</v-icon>Run
     </v-btn>
-    <v-btn fab small color="primary" @click="$root.$emit('step')">
-      <v-icon color="white">mdi-step-forward</v-icon>
+    <v-btn rounded color="primary" @click="$root.$emit('step')" class="mr-1">
+      <v-icon color="white">mdi-step-forward</v-icon>Step
     </v-btn>
-    <v-btn fab small color="primary" @click="$root.$emit('stop')">
-      <v-icon color="white">mdi-stop</v-icon>
+    <v-btn rounded color="primary" @click="$root.$emit('stop')" class="mr-1">
+      <v-icon color="white">mdi-stop</v-icon>Abort
     </v-btn>
-    <v-btn fab small color="primary" @click="$root.$emit('folder')">
-      <v-icon color="white">mdi-folder</v-icon>
+    <v-btn rounded color="primary" @click="$root.$emit('folder')">
+      <v-icon color="white">mdi-folder</v-icon>Load file
     </v-btn>
-    <!--<b-button @click="$emit('play')" variant="success">
-            <font-awesome-icon icon="play"></font-awesome-icon>
-        </b-button>
-        <b-button @click="$emit('stop')" variant="danger">
-            <font-awesome-icon icon="stop"></font-awesome-icon>
-        </b-button>
-        <b-button @click="$emit('file')" variant="primary">
-            <font-awesome-icon icon="folder"></font-awesome-icon>
-    </b-button>-->
-  </v-btn-toggle>
+  </div>
 </template>
 
 <script>
